ed08bebece8dd2a1b6aca5a8ed67444870fa8ec5
batista29 <natabatista2908@gmail.com>
2024-07-23 11:36:27 -0300

    Base do banco completa

--- bcd/script.sql
+++ bcd/script.sql
@@ -1,15 +1,32 @@
-drop database if exists Your_projects;
-create database Your_projects charset=UTF8 collate utf8_general_ci;
+drop database if exists my_projects;
+create database my_projects charset=UTF8 collate utf8_general_ci;
+use my_projects;
 
 create table user(
-    id integer primary key auto_increment,
-    nome varchar(30) not null,
-    email varchar(30) not null,
-    senha varchar(10) not null,
-    telefone varchar(14)
-); 
+    id_user INTEGER PRIMARY KEY auto_increment,
+    nome VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    senha VARCHAR(10) NOT NULL,
+    telefone VARCHAR(14)
+);
 
 create table project(
-    id integer primary key auto_increment,
-    nome varchar(30) not null,
-)
+    id_project INTEGER PRIMARY KEY auto_increment,
+    nome VARCHAR(30) NOT NULL
+);
+
+create table user_project(
+    id_user INTEGER,
+    FOREIGN KEY(id_user) REFERENCES user(id_user),
+    id_project INTEGER,
+    FOREIGN KEY(id_project) REFERENCES project(id_project)
+);
+
+create table task(
+    id INTEGER PRIMARY KEY auto_increment,
+    id_project INTEGER,
+    FOREIGN KEY(id_project) REFERENCES project(id_project),
+    nome VARCHAR(30) NOT NULL,
+    data_inicio DATETIME NOT NULL,
+    data_fim DATETIME
+);
