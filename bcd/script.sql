faab75a8c86d9edb5fe8f016905791c9e869a5dc
batista29 <natabatista2908@gmail.com>
2024-07-29 15:26:50 -0300

    Banco testado e quase 100%

--- bcd/script.sql
+++ bcd/script.sql
@@ -4,7 +4,7 @@ use my_projects;
 
 create table user(
     id_user INTEGER PRIMARY KEY auto_increment,
-    nome VARCHAR(30) NOT NULL,
+    nome_user VARCHAR(30) NOT NULL,
     email VARCHAR(30) NOT NULL,
     senha VARCHAR(10) NOT NULL,
     telefone VARCHAR(14)
@@ -12,7 +12,7 @@ create table user(
 
 create table project(
     id_project INTEGER PRIMARY KEY auto_increment,
-    nome VARCHAR(30) NOT NULL
+    nome_project VARCHAR(30) NOT NULL
 );
 
 create table user_project(
@@ -30,3 +30,26 @@ create table task(
     data_inicio DATETIME NOT NULL,
     data_fim DATETIME
 );
+
+INSERT INTO user VALUES(DEFAULT,'Nata Batista','nata@gmail.com','1234','(19)98194-0067');
+INSERT INTO user VALUES(DEFAULT,'Lucas','lucas@gmail.com','1234','(19)95194-0067');
+SELECT * FROM user;
+
+INSERT INTO project VALUES(DEFAULT,'Primeiro projeto');
+INSERT INTO project VALUES(DEFAULT,'Segundo projeto');
+INSERT INTO project VALUES(DEFAULT,'Terceiro projeto');
+SELECT * FROM project;
+
+INSERT INTO user_project VALUES(1,1);
+INSERT INTO user_project VALUES(1,2);
+INSERT INTO user_project VALUES(2,3);
+
+-- Ligação da tabela de user com projeto
+SELECT
+user.nome_user, user.email, user.telefone,
+project.nome_project
+FROM user
+INNER JOIN user_project
+ON user.id_user = user_project.id_user
+INNER JOIN project
+ON project.id_project = user_project.id_project;
